ee39349d47a9ac02ba2ce70d057c5c4f6be04a8a
Liza <ev2154@gmail.com>
2016-11-04 21:44:52 -0400

    fixed server port start

--- server.js
+++ server.js
@@ -178,6 +178,6 @@ app.get('/getProfileData/:user', function(req, res) {
 /**
  * Start regular server
  */
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
